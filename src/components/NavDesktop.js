d4d1c7bb4a78966ba74d9c1570d550ce58c7b973
Daniel G <dgimenez123@gmail.com>
2023-01-22 17:11:13 +0100

    logins terminados

--- src/components/NavDesktop.js
+++ src/components/NavDesktop.js
@@ -6,7 +6,7 @@ import { HiOutlineX } from 'react-icons/hi';
 import { FaAlignJustify, } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { token } from './functions/Auth';
-
+import { useEffect } from 'react';
 
 
 
@@ -14,11 +14,17 @@ import { token } from './functions/Auth';
 function NavDesktop() {
     const [isOpen, setIsOpen] = useState(false)
     const handleClick = () => setIsOpen(!isOpen)
-    const handleSesion=()=>{ localStorage.removeItem('token')}
+    const handleSesion = () => { localStorage.removeItem('token') }
 
+    useEffect(() => {
+       }, []);
+       
+         
+        
+  
+    
 
-
-
+    
 
 
 
@@ -28,7 +34,8 @@ function NavDesktop() {
         <>
 
             <div className="   shadow-md w-full fixed z-10 ">
-                <div className="  hidden lg:block bg-slate-800 text-gray-300 py-2 text-sm">
+
+                {!token() ? (<div className="  hidden  bg-slate-800 text-gray-300 py-2 text-sm">
                     <div className=" container  mx-auto flex justify-between">
                         <div className="flex items-center">
                             <div>
@@ -41,8 +48,23 @@ function NavDesktop() {
                             <a href="" className=" bg-do-blue-mediumdark px-3 py-1 rounded text-sky-400 font-semibold">We're Hiring</a>
 
                         </div>
-                    </div>
-                </div>
+                    </div> </div>) : (<div className="  hidden lg:block bg-slate-800 text-gray-300 py-2 text-sm"><div className=" container  mx-auto flex justify-between">
+                        <div className="flex items-center">
+                            <div>
+                                Ne donnez pas suite aux demandes de prise de contact en dehors de la plateforme laissées dans des
+                                commentaires!
+
+                            </div>
+                        </div>
+                        <div className="flex gap-x-4 items-center">
+                            <a href="" className=" bg-do-blue-mediumdark px-3 py-1 rounded text-sky-400 font-semibold">We're Hiring</a>
+
+                        </div>
+                    </div> </div>)}
+
+                
+
+               
 
                 <div className="  flex items-center  mx-auto py-5 w-full bg-slate-50">
                     <div className=" container mx-auto flex items-center gap-x-6 ">
@@ -65,14 +87,14 @@ function NavDesktop() {
                             <div className='hidden items-center g md:block'>
                                 <div className=" w-full h-14 flex justify-around gap-10">
 
-                                    
-                               {
-                                !token() ? ( <a className=" hover:shadow-lg shadow-md shadow-gray-500/50 border w-40 text-center py-3 rounded-xl  duration-300 ease-out hover:ease-in" href="/login">
-                                Log In</a>):(<a onClick={handleSesion} href="/" className=" w-40 h-14 px-5 py-3 rounded-xl hover:shadow-md shadow-lg shadow-blue-800  bg-blue-700 hover:bg-blue-700 active:bg-blue-900 text-white font-bold transition duration-300 ease-out hover:ease-in text-center">Log out</a>
-                            )
-                               } 
-                                   
-                                        </div>
+
+                                    {
+                                        !token() ? (<a className=" hover:shadow-lg shadow-md shadow-gray-500/50 border w-40 text-center py-3 rounded-xl  duration-300 ease-out hover:ease-in" href="/login">
+                                            Log In</a>) : (<a onClick={handleSesion} href="/" className=" w-40 h-14 px-5 py-3 rounded-xl hover:shadow-md shadow-lg shadow-blue-800  bg-blue-700 hover:bg-blue-700 active:bg-blue-900 text-white font-bold transition duration-300 ease-out hover:ease-in text-center">Log out</a>
+                                        )
+                                    }
+
+                                </div>
                             </div>
                         </div>
                         <div className=' px-4 '>
@@ -113,7 +135,7 @@ function NavDesktop() {
                             <button className="mt-3 mx-16 hover:shadow-lg shadow-md shadow-gray-500/50 border px-5 py-3 rounded-xl  duration-300 ease-out hover:ease-in">
                                 <a onClick={handleSesion} className=' p-0 hover:text-gray-800'>Log out</a>
                             </button>
-                            <button href ="/login" className='mb-4 mx-16 px-5 py-3 rounded-xl hover:shadow-md shadow-lg shadow-blue-800  bg-blue-700 hover:bg-blue-700 active:bg-blue-900 text-white font-bold transition duration-300 ease-out hover:ease-in'>Log in</button>
+                            <button href="/login" className='mb-4 mx-16 px-5 py-3 rounded-xl hover:shadow-md shadow-lg shadow-blue-800  bg-blue-700 hover:bg-blue-700 active:bg-blue-900 text-white font-bold transition duration-300 ease-out hover:ease-in'>Log in</button>
                         </div>
                     </ul>
                 </div>
